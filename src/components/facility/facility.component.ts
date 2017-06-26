eca7ab043c8aefd738efa44c97e6954f85060fa5
dnefedchenko <dnefedchenko@gmail.com>
2017-06-26 09:41:15 +0300

    Form controls bounded

--- src/components/facility/facility.component.ts
+++ src/components/facility/facility.component.ts
@@ -10,10 +10,71 @@ import {Response} from "@angular/http";
 export class FacilityComponent implements OnInit {
     pageLoadingComplete: boolean = false;
 
+    temperatureUnitOptions: Array<any>;
+    bloodPressureUnitOptions: Array<any>;
+    dateFormatOptions: Array<any>;
+    timeFormatOptions: Array<any>;
+    accessTimeoutOptions: Array<any>;
+    noiseFrequencyOptions: Array<any>;
+    screenTimeoutOptions: Array<any>;
+    customDelayOptions: Array<any>;
+
     facility: any;
     facilityForm: FormGroup;
 
     constructor(private apiService: ApiService, private formBuilder: FormBuilder) {
+        this.temperatureUnitOptions = [
+            {name: 'Celsius'},
+            {name: 'Fahrenheit'}
+        ];
+
+        this.bloodPressureUnitOptions = [
+            {name: 'mmHg'},
+            {name: 'kPa'}
+        ];
+
+        this.dateFormatOptions  = [
+            {name: 'MON_DD_YYYY', alias: 'MMM dd, yyyy'},
+            {name: 'DD_MON_YYYY', alias: 'dd MMM yyyy'},
+            {name: 'MM_DD_YYYY', alias: 'MM/dd/yyyy'},
+            {name: 'DD_MM_YYYY', alias: 'dd/MM/yyyy'}
+        ];
+
+        this.timeFormatOptions = [
+            {name: 'HOUR_12', alias: '12 Hour'},
+            {name: 'HOUR_24', alias: '24 Hour'}
+        ];
+
+        this.accessTimeoutOptions = [
+            {name: 'ONE_MINUTE', alias: '1min'},
+            {name: 'TWO_MINUTES', alias: '2min'},
+            {name: 'THREE_MINUTES', alias: '3min'},
+            {name: 'FOUR_MINUTES', alias: '4min'},
+            {name: 'FIVE_MINUTES', alias: '5min'},
+            {name: 'SIX_MINUTES', alias: '6min'},
+            {name: 'SEVEN_MINUTES', alias: '7min'},
+            {name: 'EIGHT_MINUTES', alias: '8min'},
+            {name: 'NINE_MINUTES', alias: '9min'},
+            {name: 'TEN_MINUTES', alias: '10min'}
+        ];
+
+        this.noiseFrequencyOptions = [
+            {name: 'SIXTY', alias: '60 Hz'},
+            {name: 'FIFTY', alias: '50 Hz'}
+        ];
+
+        this.screenTimeoutOptions = [
+            {alias: 'Off', name: 'OFF'},
+            {alias: '15sec', name: 'FIFTEEN_SECONDS'},
+            {alias: '30sec', name: 'THIRTY_SECONDS'},
+            {alias: '1min', name: 'ONE_MINUTE'},
+            {alias: '3min', name: 'THREE_MINUTES'}
+        ];
+
+        this.customDelayOptions = [
+            {name: '0', alias: 'Default',},
+            {name: '1', alias: 'Wake Forest'}
+        ];
     }
 
     ngOnInit(): void {
